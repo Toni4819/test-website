ecb23ffc746b5108b619be548388fd38793660ed
Toni_4819 <132385090+Toni4819@users.noreply.github.com>
2025-09-02 14:39:09 +0200

    a

--- scripts/data.js
+++ scripts/data.js
@@ -2,31 +2,13 @@ const objets3D = [
   {
     id: "dragon",
     nom: "Dragon 3D",
-    image: "images/dragon.jpg",
+    image: "https://cdn-acop.makeronline.com/asop/2024-10/10/webp/172850401991196900-6706e0d3dea6.webp",
     description: "Une magnifique figurine de dragon imprimée en 3D.",
   },
   {
     id: "vaisseau",
     nom: "Vaisseau spatial",
-    image: "images/vaisseau.jpg",
+    image: "https://cdn-acop.makeronline.com/asop/2024-10/10/webp/172850401991196900-6706e0d3dea6.webp",
     description: "Un vaisseau futuriste pour les fans de science-fiction.",
-  },
-  {
-    id: "tour",
-    nom: "Tour médiévale",
-    image: "images/tour.jpg",
-    description: "Une tour médiévale détaillée, parfaite pour les dioramas.",
-  },
-  {
-    id: "robot",
-    nom: "Robot articulé",
-    image: "images/robot.jpg",
-    description: "Un robot articulé avec des pièces mobiles.",
-  },
-  {
-    id: "chat",
-    nom: "Chat mignon",
-    image: "images/chat.jpg",
-    description: "Un petit chat adorable imprimé en PLA.",
   }
 ];
